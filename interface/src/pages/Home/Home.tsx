9b93bfcd0869a736afa07719b806bf35c6237bde
Eddie <21100419@virtualwindow.co.za>
2024-10-01 02:46:06 +0200

    Success testing .py engine & ui link
    The python engine can communicate with the interface successfully. Just testing text communication.

--- interface/src/pages/Home/Home.tsx
+++ interface/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import TestCommunication from "../Tests/EngineLinkTest";
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <TestCommunication />
     </div>
   );
 };
